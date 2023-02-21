fa4d1889ce06c49df39e2671ef62f007a039dde7
Tommy Zimmerman <tomzimmerman7@protonmail.com>
2023-02-21 11:12:30 -0500

    Added hint to the rule

--- lottery.js
+++ lottery.js
@@ -1,1 +1,3 @@
-//Write a javascript program that resembles the "JSexercise" gif as closely as possible using Javascript, HTML, and CSS.
+//Write a javascript program that resembles the "JSexercise" gif as closely as possible using Javascript, HTML, and CSS/Bootstrap.
+//* Hint: setInterval() function is required in order to make the number change on an interval!
+
